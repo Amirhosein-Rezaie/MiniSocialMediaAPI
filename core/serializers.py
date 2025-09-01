06f592669bbce3054bde6f3d5ea3e4281b818036
Amirhosein <ah.rezaie07@gmail.com>
2025-09-01 12:41:34 -0700

    create serilizers in core app

---
+++ core/serializers.py
@@ -0,0 +1,47 @@
+from rest_framework.serializers import (
+    ModelSerializer, CharField
+)
+from core import models as CoreModels
+
+
+class UsersSerializer(ModelSerializer):
+    password = CharField(write_only=True)
+
+    class Meta:
+        model = CoreModels.Users
+        fields = '__all__'
+
+
+class TextsSerializer(ModelSerializer):
+    user_detail = UsersSerializer(read_only=True, source='user')
+
+    class Meta:
+        model = CoreModels.Texts
+        fields = '__all__'
+
+
+class VideosSerializer(ModelSerializer):
+    user_detail = UsersSerializer(read_only=True, source='user')
+
+    class Meta:
+        model = CoreModels.Videos
+        fields = '__all__'
+
+
+class ImagesSerializer(ModelSerializer):
+    user_detail = UsersSerializer(read_only=True, source='user')
+
+    class Meta:
+        model = CoreModels.Images
+        fields = '__all__'
+
+
+class PostsSerializer(ModelSerializer):
+    user_detail = UsersSerializer(read_only=True, source='user')
+    text_detail = UsersSerializer(read_only=True, source='text')
+    video_detail = UsersSerializer(read_only=True, source='video')
+    image_detail = UsersSerializer(read_only=True, source='user')
+
+    class Meta:
+        model = CoreModels.Posts
+        fields = '__all__'
