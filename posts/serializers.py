0049afb6fb74854a6c7a41d206f4262b91bccec8
Amirhosein <ah.rezaie07@gmail.com>
2025-09-12 01:03:50 -0700

    create a custom serialzier to serialize [album, [posts]] data.

--- posts/serializers.py
+++ posts/serializers.py
@@ -1,5 +1,5 @@
 from rest_framework.serializers import (
-    ModelSerializer
+    ModelSerializer, Serializer
 )
 from posts import models as PostsModels
 from core.serializers import (
@@ -50,3 +50,33 @@ class ViewPostSerializer(ModelSerializer):
     class Meta:
         model = PostsModels.ViewPost
         exclude = ['user']
+
+
+class AlbumWithPostSerializer(Serializer):
+    """
+    Custom serializer for returning an album along with its posts.
+    The output format will be a dictionary:
+        {
+            "album": { ...album data... },
+            "posts": [ ...list of post data... ]
+        }
+    """
+
+    def to_representation(self, instance):
+        """
+        Convert a tuple (album, posts) into a dictionary representation.
+
+        Steps:
+        1. Unpack the tuple into `album` and `posts`.
+        2. Serialize the album instance using AlbumsSerializer.
+        3. Serialize the list of posts using PostsSerializer with many=True.
+        4. Return a dictionary with keys 'album' and 'posts'.
+        """
+        album, posts = instance  # unpack the tuple
+        album_data = AlbumsSerializer(album).data  # serialize the album
+        # serialize all related posts
+        posts_data = PostsSerializer(posts, many=True).data
+        return {
+            'album': album_data,  # key 'album' holds serialized album data
+            'posts': posts_data   # key 'posts' holds list of serialized posts
+        }
